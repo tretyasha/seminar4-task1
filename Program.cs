ad7b42873c41ba77ff327be9ec6b3a7eb2e1b8d2
tretyasha <72045043+tretyasha@users.noreply.github.com>
2023-02-16 21:05:01 +0300

    Задача1

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Первое число ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number == number2 * number2) Console.WriteLine("Первое число является квадратом второго");
+else Console.WriteLine("Печаль");
